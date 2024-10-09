684f906b2aa3d7e724de23e28c541e1b62a5ebe7
rajonahmed9650 <rajon9650@gmail.com>
2024-10-09 10:03:30 +0600

    To do list

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector('#push').onclick = function
+    () {
+    if (document.querySelector('#newtask input ').
+        value.length == 0) {
+        alert("Please Enter a Task")
+    } else {
+        document.querySelector('#tasks').innerHTML
+            += `
+        <div class = 'task'>
+               <span class ="taskname">${document.querySelector("#newtask input").value}</span>
+               <button class = "delete">
+                <i class="fa-solid fa-trash-can"></i>
+                </button>
+        </div>
+        `;
+        var  current_tasks = document.querySelectorAll('.delete');
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove()
+            }
+            
+        }
+        var tasks = document.querySelectorAll(".task")
+        for(var i = 0 ; i <tasks.length ; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('comleted')
+            }
+            
+        }
+        document.querySelector('#newtask input').value = ""
+    }
+
+}   
